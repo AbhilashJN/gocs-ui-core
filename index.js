de63f07b6c64bffc571a307c527b3c2197dfb04a
Abhilash Nambissan <abhilashjn107@gmail.com>
2021-07-20 14:14:00 +0530

    export components

---
+++ index.js
@@ -0,0 +1,15 @@
+import HeatmapsPage from './src/components/HeatmapsPage'
+import Home from './src/components/Home'
+import Loader from './src/components/Loader'
+import NavBar from './src/components/NavBar'
+import PlayersList from './src/components/PlayersList'
+import StatsPage from './src/components/StatsPage'
+import StatTable from './src/components/StatTable'
+
+export const HeatmapsPage
+export const Home
+export const Loader
+export const NavBar
+export const PlayersList
+export const StatsPage
+export const StatTable
